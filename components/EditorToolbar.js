ee53f4c8a542e7ddb25633f5dfc4f862116459d8
Paweł Służalec <pawel.sluzalec@gmail.com>
2016-06-28 15:44:12 +0200

    Further styling

--- components/EditorToolbar.js
+++ components/EditorToolbar.js
@@ -3,22 +3,26 @@ import React from 'react';
 class EditorToolbar extends React.Component {
   render() {
     return (
-      <div>
-        <span>Day</span>
-        <select name="day" >
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-        </select>
-        <span>Meal</span>
-        <select name="meal">
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-        </select>
-        <button>Add item</button>
-        <button>Add meal</button>
-        <button>Add day</button>
+      <div className="Toolbar">
+        <div className="toolbar-selects">
+          <span>Day</span>
+          <select name="day" >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+          <span>Meal</span>
+          <select name="meal">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+        </div>
+        <div className="toolbar-buttons">
+          <button>Add item</button>
+          <button>Add meal</button>
+          <button>Add day</button>
+        </div>
       </div>
     )
   }
--- components/Home.js
+++ components/Home.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 class Home extends React.Component {
   render() {
-    return <h1>Home</h1>
+    return (
+      <div className="content">
+        <h1>Welcome!</h1>
+      </div>
+    )
   }
 }
 
